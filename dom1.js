eaea39cb7906d535232bc63dd7a4d1deaec285bf
abhi3451 <abhi3451@gmail.com>
2022-11-24 23:38:27 +0530

    A

--- dom1.js
+++
@@ -1,8 +0,0 @@
-console.dir(document)
-var element=document.getElementsByClassName('list-group-item')
-console.log(element[i])
-element[2].style.background="green"
-for(var i=0;i<element.length;i++)
-{
-    element[i].style.fontWeight="bold"
-}
