59d36cdb77fbe5e93811b5d860936d00c57adc67
abhi3451 <abhi3451@gmail.com>
2022-11-24 23:32:21 +0530

    first commit

---
+++ dom1.js
@@ -0,0 +1,8 @@
+console.dir(document)
+var element=document.getElementsByClassName('list-group-item')
+console.log(element[i])
+element[2].style.background="green"
+for(var i=0;i<element.length;i++)
+{
+    element[i].style.fontWeight="bold"
+}
