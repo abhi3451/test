40425202cdd2a3cb0b98d9a4237609314087eebe
abhi3451 <abhi3451@gmail.com>
2022-11-25 14:52:58 +0530

    first commit

--- file1/dom1.js
+++ file1/dom1.js
@@ -20,3 +20,23 @@ console.dir(document)
 //element.textContent="hello"
 //console.log(element)
 //element.style.background= "yellow"
+ 
+
+//Queryselector
+// var seconditem=document.querySelector('.list-group-item:nth-child(2)')
+// seconditem.style.background="green"
+
+
+
+//QuerySelectorall
+//  var item=document.querySelectorAll('li:nth-child(2)')
+//  var odd=document.querySelectorAll('li:nth-child(odd)')
+//   for(var i=0;i<odd.length;i++)
+//  {
+//     odd[i].style.backgroundColor="grey"
+//     item[i].style.color="red"
+    
+
+//  }
+ 
+ 
