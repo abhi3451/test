58474c09778ad05eb654140a652fd2e46b92411c
abhi3451 <abhi3451@gmail.com>
2022-11-25 13:45:03 +0530

    second commit

--- file1/dom1.js
+++ file1/dom1.js
@@ -1,8 +1,22 @@
 console.dir(document)
-var element=document.getElementsByClassName('list-group-item')
-console.log(element[i])
-element[2].style.background="green"
-for(var i=0;i<element.length;i++)
-{
-    element[i].style.fontWeight="bold"
-}
+// var element=document.getElementsByClassName("list-group-item")
+// console.log(element[i])
+// element[2].style.background="green"
+// for(var i=0;i<element.length;i++)
+// {
+//     element[i].style.fontWeight="bold"
+// }
+
+//getelement by tag name and class name 
+
+// var element=document.getElementsByTagName('li')
+// element.style.background="yellow"
+// console.log(element[4])
+// element[4].style.background="yellow"
+// element[4].style.fontWeight="bold"
+// element[4].style.fontStyle="italic"
+
+//var element=document.getElementsByClassName("xyz")
+//element.textContent="hello"
+//console.log(element)
+//element.style.background= "yellow"
